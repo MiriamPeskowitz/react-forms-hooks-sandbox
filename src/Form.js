72ac4078ccbeca00f81a63a0b5464dbfd440187a
MiriamPeskowitz <miriam5000@gmail.com>
2021-04-03 21:41:14 -0400

    added

---
+++ src/Form.js
@@ -0,0 +1,53 @@
+//stateful functional component 
+import React from 'react'
+import useForm from "./useForm"
+
+const Form = () => {
+	const {values, handleChange, handleSubmit } = useForm()
+
+	function login() {
+		console.log(values)
+	}
+
+	return (
+		<div className="section is-fullheight">
+			<div className="container">
+				<div className="colum is-4 is-offset-4">
+					<div className="box">
+						<form onSubmit={handleSubmit}>
+							<div className="field">
+								<label className="label">Email address</label>
+								<div className="control">
+									<input 
+										className="input"
+										type="email"
+										name="email"
+										onChange={handleChange}
+										value={values.email}
+										required
+									/>
+								</div>
+							</div>
+							<div className="field">
+								<label className="label">Password</label>
+								<div className="control">
+									<input 
+										className="input"
+										type="password"
+										name="password"
+										onChange={handleChange}
+										value={values.password}
+										required
+									/>
+								</div>
+							</div>
+							<button type="submit" className="button is-block is-info is-fullwidth">Login</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+		)
+}
+
+export default Form;
---
+++ src/useForm.js
@@ -0,0 +1,35 @@
+//custom hooks -- convention is use in fron of the name, , so React knows the file is a hook 
+import {useState} from 'react' //keep track of form values 
+
+//useForm takes one paramater -- callback 
+const useForm = (callback) => {
+
+	const [values, setValues] = useState({})
+	
+	const handleSubmit = (event) => {
+		if (event) event.preventDefault
+			callback()
+	}
+
+
+
+	const handleChange = (event) => {
+		event.persist()
+		//called when using React synthetic events inside async callback function 
+		//synth event -- wrapper, sim to stopProgragation and preventDefault 
+		//notused after v17, because no pooling 
+		setValues(values => (
+			{ ...values, 
+				[event.target.name: event.target.value]
+
+			}))
+		}
+		
+		return {
+			handleChange, 
+			handleSubmit,
+			values,
+		}
+	}
+
+	export default useForm; 
